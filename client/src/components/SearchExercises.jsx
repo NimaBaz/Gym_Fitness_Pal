fce95c40faaf1a5890858a8dc4d52628d6455220
NimaBaz <32779333+NimaBaz@users.noreply.github.com>
2023-07-12 13:40:10 -0700

    projects updates

--- client/src/components/SearchExercises.jsx
+++ client/src/components/SearchExercises.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 
 const SearchExercises = () => {
+    
+
     return (
         <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
             <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="50px" textAlign="center">
@@ -9,9 +11,21 @@ const SearchExercises = () => {
             </Typography>
 
             <Box position="relative" mb="72px">
-                <TextField height="76px" value="" onChange={ (e) => {} }>
-
+                <TextField 
+                    height="76px" 
+                    sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px'}, 
+                        width: { lg: '800px', xs: '350px' }, 
+                        backgroundColor: '#FFF',
+                        borderRadius: '40px'
+                    }}
+                    value="" 
+                    onChange={ (e) => {} } 
+                    placeholder='Search Exercises' 
+                    type='text'>
                 </TextField>
+                <Button className='search-btn' sx={{ bgcolor: '#FF2625', color: '#FFF', textTransform: 'none', width: { lg: '175px', xs: '80px'}, fontSize: { lg: '20px', xs: '14px' }, height: '56px', position: 'absolute', right: '0' }}>
+                    Search
+                </Button>
             </Box>
         </Stack>
     )
